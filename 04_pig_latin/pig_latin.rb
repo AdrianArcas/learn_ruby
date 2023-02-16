e557064beaea8a3c47eed2525c55a82ed9c23eb9
Adrian Arcas <102859722+AdrianArcas@users.noreply.github.com>
2023-02-16 22:26:20 +0200

    Generalize counting groups of letters phonemes

--- 04_pig_latin/pig_latin.rb
+++ 04_pig_latin/pig_latin.rb
@@ -1,38 +1,78 @@
 def translate(sentence)
-vowels = ['a','e','i','o','u','A','E','I','O','U']
 
-words = sentence.split()
-sentence = ""
-for word in words
-    cap = false
-
-    if word == word.capitalize
-        cap=true
+    words = sentence.split()
+    sentence = ""
+    for word in words
+  
+      cap = false
+      if word == word.capitalize
+        cap = true
+      end
+  
+      phonemes_intervals = phonemes_intervals(word)
+      curr_vowel_index = first_vowel_after_index(word, 0)
+  
+      while true
+        if vowel_outside_phonemes(curr_vowel_index, phonemes_intervals)
+  
+          if curr_vowel_index != 0
+          str_till_vowel = word[0..curr_vowel_index - 1]
+          else
+          str_till_vowel = ""
+          end
+          str_after_vowel = word[curr_vowel_index..-1]
+          word.replace str_after_vowel + str_till_vowel
+          break
+        else
+          curr_vowel_index = first_vowel_after_index(word, curr_vowel_index + 1)
+        end
+      end
+  
+      word.replace word.downcase
+      word << "ay"
+  
+      if cap
+        word.replace word.capitalize
+      end
+  
+      cap = false
+      sentence << word + " "
+  
     end
-
-    if not vowels.include?(word[0]) 
-       while not vowels.include?(word[0]) or ((word[0]=='u' or word[0]=='U') and (word[-1]=='q' or word[-1] == 'Q'))
-            word << word[0] 
-            delete_first_char(word)
-       end
+  
+      return sentence.rstrip
+  end
+  
+  def phonemes_intervals(word)
+    phonemes = %w[qu sch Qu Sch]
+    phonemes_intervals=[]
+  
+    phonemes.each do |phoneme|
+      phoneme_index = word.index(phoneme)
+  
+      if phoneme_index
+        phonemes_intervals.append([phoneme_index,phoneme_index+phoneme.length-1])
+  
+      end
     end
-    word= word.downcase
-    word << "ay"
-
-    if cap
-    word=word.capitalize
+    return phonemes_intervals
+  end
+  
+  def first_vowel_after_index(word, starting_index)
+    vowels = %w[a e i o u A E I O U]
+    for i in starting_index..word.length-1
+      if vowels.include?(word[i])
+        return i
+      end
     end
-    cap=false
-    
-    sentence << word + " "
-    
-end
-return sentence.rstrip
-end
-
-def delete_first_char(str)
-    for i in 0..str.length-2
-        str[i] = str [i+1]
+  end
+  
+  def vowel_outside_phonemes(vowel_index, phonemes_intervals)
+    phonemes_intervals.each do |interval|
+      if vowel_index >= interval[0] and vowel_index <= interval[1]
+        return false
+      end
     end
-    str.replace(str.chop)
-end
+    return true
+  end
+  
