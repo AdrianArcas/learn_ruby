1cfa434d522c1089c8fe566af0df91c3420c471b
Adrian Arcas <102859722+AdrianArcas@users.noreply.github.com>
2023-02-21 00:35:25 +0200

    Refactor title= and make  a constant

--- 08_book_titles/book.rb
+++ 08_book_titles/book.rb
@@ -1,24 +1,19 @@
-$non_capital_words = %w[a the an] + %w[for and nor but or yet] + %w[above across against along among around at before behind 
+NON_CAPITAL_WORDS = %w[a the an] + %w[for and nor but or yet] + %w[above across against along among around at before behind
 below beneath beside between by down from in into near of off on to toward under upon with]
+
 class Book
-    def title=(title)
-      words=title.split
-  
-       words.map!{
-        |word|
-        unless $non_capital_words.include?(word)
-        word.replace word.capitalize
-        else
+  def title=(title)
+
+    @title = title.split.map.with_index {|word,i|
+      unless NON_CAPITAL_WORDS.include? word and i!=0
+        word.capitalize
+      else
         word
-        end
-      }
-  
-      words[0].capitalize!
-  
-      @title = words.join(" ")
-  
-    end
-    def title
-      @title
-    end
+      end }.join(" ")
   end
+
+  def title
+    @title
+  end
+  
+end
