1e684d19dfaba44419708f2cae38811f668fb426
Adrian Arcas <102859722+AdrianArcas@users.noreply.github.com>
2023-02-21 00:58:06 +0200

    Add attr_reader

--- 08_book_titles/book.rb
+++ 08_book_titles/book.rb
@@ -2,8 +2,9 @@ NON_CAPITAL_WORDS = %w[a the an] + %w[for and nor but or yet] + %w[above across 
 below beneath beside between by down from in into near of off on to toward under upon with]
 
 class Book
-  def title=(title)
+  attr_reader :title
 
+  def title=(title)
     @title = title.split.map.with_index {|word,i|
       unless NON_CAPITAL_WORDS.include? word and i!=0
         word.capitalize
@@ -12,8 +13,6 @@ class Book
       end }.join(" ")
   end
 
-  def title
-    @title
-  end
+  
   
 end
