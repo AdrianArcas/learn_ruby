50b242067987c7c3f37eda76086747a260acde9d
Adrian Arcas <102859722+AdrianArcas@users.noreply.github.com>
2023-02-20 23:58:30 +0200

    Refactor loops

--- 04_pig_latin/pig_latin.rb
+++ 04_pig_latin/pig_latin.rb
@@ -1,78 +1,80 @@
 def translate(sentence)
 
-    words = sentence.split()
-    sentence = ""
-    for word in words
-  
-      cap = false
-      if word == word.capitalize
-        cap = true
-      end
-  
-      phonemes_intervals = phonemes_intervals(word)
-      curr_vowel_index = first_vowel_after_index(word, 0)
-  
-      while true
-        if vowel_outside_phonemes(curr_vowel_index, phonemes_intervals)
-  
-          if curr_vowel_index != 0
+  sentence.split.map { |word|
+    cap = false
+    if word == word.capitalize
+      cap = true
+    end
+
+    phonemes_intervals = phonemes_intervals(word)
+    curr_vowel_index = first_vowel_after_index(word, 0)
+
+    while true
+      if vowel_outside_phonemes(curr_vowel_index, phonemes_intervals)
+
+        if curr_vowel_index != 0
           str_till_vowel = word[0..curr_vowel_index - 1]
-          else
-          str_till_vowel = ""
-          end
-          str_after_vowel = word[curr_vowel_index..-1]
-          word.replace str_after_vowel + str_till_vowel
-          break
         else
-          curr_vowel_index = first_vowel_after_index(word, curr_vowel_index + 1)
+          str_till_vowel = ""
         end
-      end
-  
-      word.replace word.downcase
-      word << "ay"
-  
-      if cap
-        word.replace word.capitalize
-      end
-  
-      cap = false
-      sentence << word + " "
-  
-    end
-  
-      return sentence.rstrip
-  end
-  
-  def phonemes_intervals(word)
-    phonemes = %w[qu sch Qu Sch]
-    phonemes_intervals=[]
-  
-    phonemes.each do |phoneme|
-      phoneme_index = word.index(phoneme)
-  
-      if phoneme_index
-        phonemes_intervals.append([phoneme_index,phoneme_index+phoneme.length-1])
-  
+        str_after_vowel = word[curr_vowel_index..-1]
+        word.replace str_after_vowel + str_till_vowel
+        break
+      else
+        curr_vowel_index = first_vowel_after_index(word, curr_vowel_index + 1)
       end
     end
-    return phonemes_intervals
+
+    word.downcase!
+    word << "ay"
+
+    if cap
+      word.capitalize!
+    else
+      word
+    end
+    }.join(" ")
+
+end
+
+def phonemes_intervals(word)
+  phonemes = %w[qu sch Qu Sch]
+  phonemes_intervals=[]
+
+  phonemes.each do |phoneme|
+    phoneme_index = word.index(phoneme)
+
+    if phoneme_index
+      phonemes_intervals.append([phoneme_index,phoneme_index+phoneme.length-1])
+    end
+
   end
-  
-  def first_vowel_after_index(word, starting_index)
-    vowels = %w[a e i o u A E I O U]
-    for i in starting_index..word.length-1
-      if vowels.include?(word[i])
-        return i
-      end
+  return phonemes_intervals
+end
+
+def first_vowel_after_index(word, starting_index)
+
+  vowels = %w[a e i o u A E I O U]
+  word[starting_index..-1].split("").each.with_index(starting_index) do |letter,i|
+    return i if vowels.include?(letter)
+  end
+
+end
+
+def vowel_outside_phonemes(vowel_index, phonemes_intervals)
+  phonemes_intervals.each do |interval|
+    if vowel_index >= interval[0] and vowel_index <= interval[1]
+      return false
     end
   end
-  
-  def vowel_outside_phonemes(vowel_index, phonemes_intervals)
-    phonemes_intervals.each do |interval|
-      if vowel_index >= interval[0] and vowel_index <= interval[1]
-        return false
-      end
-    end
-    return true
-  end
-  
+  return true
+end
+
+
+
+
+
+
+
+
+
