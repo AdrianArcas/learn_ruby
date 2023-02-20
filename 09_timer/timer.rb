12a3834698add314db10bc75880272a3d37c7026
Adrian Arcas <102859722+AdrianArcas@users.noreply.github.com>
2023-02-21 00:55:31 +0200

    Add attr_accessor for getter and setter

--- 09_timer/timer.rb
+++ 09_timer/timer.rb
@@ -1,29 +1,24 @@
 class Timer
-    def initialize()
-      @seconds = 0
-    end
-    def seconds=(seconds)
-      @seconds = seconds
-    end
-  
-    def seconds
-      @seconds
-    end
-  
-    def time_string
-      seconds = @seconds%60
-      minutes = (@seconds/60)%60
-      hours = (@seconds/3600)%60
-  
-      @time_string = "#{padded(hours)}:#{padded(minutes)}:#{padded(seconds)}"
-    end
-  
-    def padded(n)
-      if n < 10
-        "0#{n}"
-      else
-        "#{n}"
-      end
-    end
-    
+
+  attr_accessor :seconds
+
+  def initialize()
+    @seconds = 0
   end
+
+  def time_string
+    seconds = @seconds%60
+    minutes = (@seconds/60)%60
+    hours = (@seconds/3600)%60
+    @time_string = "#{padded(hours)}:#{padded(minutes)}:#{padded(seconds)}"
+  end
+
+  def padded(n)
+    if n < 10
+      "0#{n}"
+    else
+      "#{n}"
+    end
+  end
+
+end
