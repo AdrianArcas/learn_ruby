659b0d3d2a38dd7c3acbbf6315ceb5fae53856de
Adrian Arcas <102859722+AdrianArcas@users.noreply.github.com>
2023-02-21 23:16:58 +0200

    Move logic rules from translate to translate_word and refactor

--- 04_pig_latin/pig_latin.rb
+++ 04_pig_latin/pig_latin.rb
@@ -1,61 +1,26 @@
 def translate(sentence)
-
-  sentence.split.map { |word|
-    cap = false
-    if word == word.capitalize
-      cap = true
-    end
-
-    phonemes_intervals = phonemes_intervals(word)
-    curr_vowel_index = first_vowel_after_index(word, 0)
-
-    while true
-      if vowel_outside_phonemes(curr_vowel_index, phonemes_intervals)
-
-        if curr_vowel_index != 0
-          str_till_vowel = word[0..curr_vowel_index - 1]
-        else
-          str_till_vowel = ""
-        end
-        str_after_vowel = word[curr_vowel_index..-1]
-        word.replace str_after_vowel + str_till_vowel
-        break
-      else
-        curr_vowel_index = first_vowel_after_index(word, curr_vowel_index + 1)
-      end
-    end
-
-    word.downcase!
-    word << "ay"
-
-    if cap
-      word.capitalize!
-    else
-      word
-    end
-    }.join(" ")
-
+  sentence.split.map { |word| translate_word(word) }.join(" ")
 end
 
 def phonemes_intervals(word)
   phonemes = %w[qu sch Qu Sch]
-  phonemes_intervals=[]
+  phonemes_intervals = []
 
   phonemes.each do |phoneme|
     phoneme_index = word.index(phoneme)
 
     if phoneme_index
-      phonemes_intervals.append([phoneme_index,phoneme_index+phoneme.length-1])
+      phonemes_intervals.append([phoneme_index, phoneme_index + phoneme.length - 1])
     end
 
   end
-  return phonemes_intervals
+  phonemes_intervals
 end
 
 def first_vowel_after_index(word, starting_index)
 
   vowels = %w[a e i o u A E I O U]
-  word[starting_index..-1].split("").each.with_index(starting_index) do |letter,i|
+  word[starting_index..-1].split("").each.with_index(starting_index) do |letter, i|
     return i if vowels.include?(letter)
   end
 
@@ -67,14 +32,44 @@ def vowel_outside_phonemes(vowel_index, phonemes_intervals)
       return false
     end
   end
-  return true
+  true
 end
 
+def translate_word(word)
 
+  cap = false
+  if word == word.capitalize
+    cap = true
+  end
 
+  phonemes_intervals = phonemes_intervals(word)
+  curr_vowel_index = first_vowel_after_index(word, 0)
 
+  while true
+    if vowel_outside_phonemes(curr_vowel_index, phonemes_intervals)
 
+      if curr_vowel_index == 0
+        str_till_vowel = ""
+      else
+        str_till_vowel = word[0..curr_vowel_index - 1]
+      end
+      str_after_vowel = word[curr_vowel_index..-1]
+      word.replace str_after_vowel + str_till_vowel
+      break
+    else
+      curr_vowel_index = first_vowel_after_index(word, curr_vowel_index + 1)
+    end
+  end
 
+  word.downcase
+  word << "ay"
 
+  if cap
+    word.capitalize
+  else
+    word
+  end
+end
 
+puts translate("the Quick brown Fox")
 
