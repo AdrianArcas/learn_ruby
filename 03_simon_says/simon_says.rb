b5b4ba6a3da8c6c4b8a2046625b85f62b5f9be36
Adrian Arcas <102859722+AdrianArcas@users.noreply.github.com>
2023-02-23 16:25:13 +0200

    Optimize repeat function

--- 03_simon_says/simon_says.rb
+++ 03_simon_says/simon_says.rb
@@ -1,38 +1,35 @@
 def shout(str)
-    str.upcase
-  end
-  
-  def echo(str)
-    str
-  end
-  
-  def repeat(str, times=2)
-    temp = str
-    (times-1).times{
-      str = str + " " + temp
-      #str << " " + temp
-      #De ce nu merge?
-      #pare ca in momentul cand se face loop-ul se actualizeaza si parametrul temp . De ce?
-    }
-    str
-  end
-  
-  def start_of_word(str,n)
-    str[0, n]
-  end
-  
-  def first_word(str)
-    str.split()[0]
-  end
-  
-  def titleize(str)
-    little_words= ["and","the","over"]
-  
-    str.split.map.with_index do|word,i|
-      if (!little_words.include?word) || i==0
-        word.capitalize
-      else
-        word
-      end
-    end.join(" ")
-  end
+  str.upcase
+end
+
+def echo(str)
+  str
+end
+
+def repeat(str, times=2)
+  temp = String.new(str)
+  (times-1).times{
+    str << " " +temp
+  }
+  str
+end
+
+def start_of_word(str,n)
+  str[0, n]
+end
+
+def first_word(str)
+  str.split()[0]
+end
+
+def titleize(str)
+  little_words= ["and","the","over"]
+
+  str.split.map.with_index do|word,i|
+    if (!little_words.include?word) || i==0
+      word.capitalize
+    else
+      word
+    end
+  end.join(" ")
+end
